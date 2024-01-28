69a668dbdcb360d9232af2cc7a9ce186f19789dc
lady-thee <onuhtheolam6@gmail.com>
2024-01-28 22:53:10 +0100

    Chore: Refactored ASGI file

--- config/asgi.py
+++ config/asgi.py
@@ -1,27 +1,26 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from django.urls import path
 
-from api.consumers import SensorConsumer
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-django_aspi_app = get_asgi_application()
+django_asgi_app = get_asgi_application()
 
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
+from api.consumers import SensorConsumer
 application = ProtocolTypeRouter(
     {
-        "http": django_aspi_app,
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(
                 URLRouter(
                     [
                         path("ws/data/", SensorConsumer.as_asgi()),
-                        path("ws/", SensorConsumer.as_asgi()),
                     ]
                 )
             ),
